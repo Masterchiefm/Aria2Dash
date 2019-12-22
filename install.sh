0076a15786d0bc329389022420d0f95be85ba77b
test <928168990@qq.com>
2019-12-22 20:07:08 +0800

    船新版本！！！自动配置filebrowser！！！
    船新版本！！！自动配置filebrowser！！！并且全部开机自启！

--- install.sh
+++ install.sh
@@ -1,44 +1,77 @@
-#!/bin/bash
-#简单脚本，快速在自己的VPS上部署离线下载利器。
-clear
-echo "本脚本仅运行于Ubuntu、Deepin、Debian等基于Debian的Linux操作系统上"
-echo "本脚本目的："
-echo " 搭建基于 Aria2 ，并且能使用网页管理器AriaNg进行下载管理的下载平台"
-echo " 主要拿来离线下载，挂机下完东西再取回美滋滋"
-echo
-echo "注意事项："
-echo " 操作系统为上述系统，并推荐部署于境外VPS中，国内的价格过高"
-echo " 注意VPS服务商是否对版权敏感，有些不许下盗版BT资源"
-echo " 确保买的VPS空间足够装以后你要下载东西"
-echo " 本程序同样可以用在非服务器的普通电脑上"
-echo
-echo "准备开始安装。将要安装以下程序以及其依赖程序："
-echo " Aria2，用以下载； AriaNg 用以提供网页版管理工具； Apache2，开启网页服务器以及文件服务器 ；vim，文本编辑器，有需要的时候用以改配置文件"
-echo "我已知晓风险并确认进行部署。按回车继续。若不知道风险，自行百度。"
-read a
-echo "请按回车键继续。。。"
-# Update
-sudo apt update -y
-# 安装必要程序
-sudo apt-get install vim aria2 apache2 unzip git -y
+#/bin/bash
+
+while getopts ":p:p2:" opt
+do
+    case $opt in
+        p)
+        p=$OPTARG
+        ;;
+        p2)
+        p2=$OPTARG
+        ;;
+    esac
+done
+
+
+
+#判断系统是debian，Ubuntu，Fedora，cent还是手机的turmux
+cmd="apt-get"
+if [[  $(command -v apt)  ]] ; then
+        cmd="yum"
+else
+        cmd="pkg"
+
+fi
+
+git clone xxxx
+
+ip=$(curl -s https://ipinfo.io/ip)
+FilePort=8090
+
+#安装必要的包
+$cmd update -y
+$cmd screen vim aria2 apache2 unzip git curl -y
+
 # 下载AriaNg
-sudo rm -rf /tmp/Onekey-deploy_aria2
-sudo git clone https://github.com/Masterchiefm/Onekey-deploy_aria2.git /tmp/Onekey-deploy_aria2
-sudo sh /tmp/Onekey-deploy_aria2/getariang
-sudo rm -rf /var/www/html/lixian
-sudo rm -rf /var/www/html/downloads
-sudo mkdir -p /var/www/html/lixian 
-sudo mkdir -p /var/www/html/downloads
-sudo unzip /tmp/Onekey-deploy_aria2/*.zip -d /var/www/html/lixian
-sudo chmod 777 -R /var/www/html/lixian
+dir="/var/www/html"
+tmp="/tmp/Onekey-deploy_aria2"
+sudo rm -rf $tmp
+sudo git clone https://github.com/Masterchiefm/Onekey-deploy_aria2.git $tmp
+sudo sh $tmp/getariang
+sudo rm -rf $dir/lixian
+sudo rm -rf $dir/downloads
+sudo mkdir -p $dir/lixian 
+sudo mkdir -p $dir/downloads
+sudo unzip $tmp/*.zip -d $dir/lixian
+sudo chmod 777 -R $dir/lixian
+link="<a href="$ip:$FilePort" target="blank">"
+echo $dir/lixian/head.html > $dir/lixian/index.html
+echo $link >> $dir/lixian/index.html
+echo $dir/lixian/foot.html > $dir/lixian/index.html
 
-#更改为使用systemctl
-sudo cp /tmp/Onekey-deploy_aria2/aria2c /etc/init.d/
+#安装FileBrowser
+curl -fsSL https://filebrowser.xyz/get.sh | bash
+filebrowser config import $tmp/filebrowser.json
+
+
+
+#开始配置aria2
+sudo rm -rf /root/.aria2
+sudo mkdir -p /root/.aria2
+sudo rm -rf /root/.aria2/*
+sudo touch /root/.aria2/aria2.session
+sudo mv /tmp/Onekey-deploy_aria2/aria2.conf /root/.aria2/
+sudo mv /tmp/Onekey-deploy_aria2/updatetracker.sh /root/.aria2/
+sudo rm -rf ./install.sh
+clear
+secret="rpc-secret=$p"
+sudo echo $secret >> /root/.aria2/aria2.conf
+
+#设置systemctl
+sudo cp $tmp/aria2c /etc/init.d/
+sudo cp $tmp/filebrowser /etc/init.d/
 sudo chmod 777  /etc/init.d/aria2c
+sudo chmod 777  /etc/init.d/filebrowser
 sudo systemctl daemon-reload
-#配置密码
-sudo sh /tmp/Onekey-deploy_aria2/setpasswd
-
-
-
-
+sudo systemctl enable aria2c
+sudo systemctl enable filebrowser
