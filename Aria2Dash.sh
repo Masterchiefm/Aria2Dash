4c9735dff1514edbdb47fc8cb621cc763eab7f52
test <moqiqin@live.com>
2020-03-29 00:24:18 +0800

    用来获取Aria2Dash的最新版本
    用来获取Aria2Dash的最新版本，如果是fork我脚本的，请自行更改tag_name和script_url的链接。

--- Aria2Dash.sh
+++ Aria2Dash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #get the latest Aria2Dash
-api="https://api.github.com/repos/Masterchiefm/Aria2Dash/releases/latest"
+#用来获取Aria2Dash的最新版本，如果是fork我脚本的，请自行更改tag_name和script_url的链接。
 tag_name="$(curl -s https://api.github.com/repos/Masterchiefm/Aria2Dash/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g')"
 script_url="https://github.com/Masterchiefm/Aria2Dash/releases/download/$tag_name/install.sh"
 echo "the latest script is $script_url"
