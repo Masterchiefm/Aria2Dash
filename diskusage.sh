c1abf22a39e886e3e68c534a8744e1ab88db1d24
test <moqiqin@live.com>
2020-03-31 15:27:01 +0800

    每分钟刷新剩余硬盘容量

---
+++ diskusage.sh
@@ -0,0 +1,11 @@
+
+#一分钟刷新一次
+ echo "<!doctype html><head><meta charset=UTF-8><meta http-equiv=refresh content=0;url=/Disk_Usage.html></head>"
+ echo "<!doctype html><head><meta charset="UTF-8"><meta http-equiv=refresh content=20;url=/Disk_Usage.html></head> <br>" > $file
+date >>$file
+echo "<br>" >>$file
+df -h |grep Used >>$file
+echo "<br>" >>$file
+df -h |grep G >>$file
+echo "<br>" >>$file
+#df -h |grep hell >>$file
